8cd8b3c2f4b3be1cabe91b71df0c2bb205905d88
gadeterding <gadeterding@crimson.ua.edu>
2023-04-17 17:16:28 -0500

    added GUI to manager menu

--- src/spring23team2/ManagerMenu.java
+++ src/spring23team2/ManagerMenu.java
@@ -1,6 +1,5 @@
 package spring23team2;
 
-import java.util.Scanner;
 import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.event.*;
@@ -14,67 +13,6 @@ import java.awt.event.*;
 *
 */
 
-/*
-public class ManagerMenu {
-	
-	public static void main(String[] args) {
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-		System.out.println("Manager Menu");
-		System.out.println("Please enter a number to select an option from the menu:");
-		System.out.println("1. Request Provider Report");
-		System.out.println("2. Request Member Report");
-		System.out.println("3. Request Summary Report");
-		System.out.println("4. Request EFT Data Report");
-		
-		int menuOption = myObj.nextInt();  // Read user input
-		
-		switch (menuOption) {
-		case 1: // Request Provider Report  
-			System.out.println("Please enter the provider number for the report you would like to request.");
-			int providerNumber = myObj.nextInt(); // Read user input
-			Provider provider = ProviderFiles.searchProvider(providerNumber); //searches provider files for provider 
-			if(provider != null) { // If provider number is valid, generate the report
-				System.out.print("Generating Provider Report for Provider #" + providerNumber);
-				System.out.println("...");
-			}
-			else { // If provider number is invalid, exit
-				System.out.println("Invalid Provider Number.");
-			}
-			break;
-		
-		case 2: // Request Member Report
-			System.out.println("Please enter the member number for the report you would like to request.");
-			int memberID = myObj.nextInt(); // Read user input
-			Member member = MemberFiles.searchMember(memberID); //searches member files for member
-			if(member != null) { // If member number is valid, generate the report
-				System.out.print("Generating Member Report for Member #" + memberID);
-				System.out.println("...");
-			}
-			else { // If member number is invalid, exit
-				System.out.println("Invalid Member Number.");
-			}
-			break;
-			
-		case 3: // Request Summary Report
-			System.out.println("Generating Summary Report...");
-			break;
-			
-		case 4: // Request EFT Data Report
-			System.out.println("Generating EFT Data Report...");
-			break;
-			
-		default: // User selected invalid option
-			System.out.println("Invalid Option");
-	    }
-		
-		System.out.println("Exiting System."); // Exits system, should return to main menu
-    }
-		
-}
-
-*/
-
-
 public class ManagerMenu extends JFrame{
 	private JButton button, button1, button2, button3;
     public ManagerMenu() {
@@ -83,13 +21,12 @@ public class ManagerMenu extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   
 
-        //add more buttons
         button = new JButton("Request Provider Report");
         button.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == button) {
+                if (e.getSource() == button) { //if "Request Provider Report" button is pressed
                     dispose(); // close current screen
-                    new ProviderReportScreen(); // open validation screen
+                    new ProviderReportScreen(); // open Provider Report screen
                 }
             }
         });
@@ -97,9 +34,9 @@ public class ManagerMenu extends JFrame{
         button1 = new JButton("Request Member Report");
         button1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == button1) {
+                if (e.getSource() == button1) { //if "Request Member Report" button is pressed
                     dispose(); // close current screen
-                    new MemberReportScreen(); // open something else screen
+                    new MemberReportScreen(); // open Member Report screen
                 }
             }    
         });
@@ -107,9 +44,9 @@ public class ManagerMenu extends JFrame{
 		button2 = new JButton("Request Summary Report");
 		button2.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == button2) {
+				if (e.getSource() == button2) { //if "Request Summary Report" button is pressed
 					dispose(); // close current screen
-					//new summaryReportScreen(); // open something else screen
+					new SummaryReportScreen(); // open Summary Report screen
 				}
 			}    
 		});
@@ -117,9 +54,9 @@ public class ManagerMenu extends JFrame{
 		button3 = new JButton("Request EFT Data Report");
 		button3.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == button3) {
+				if (e.getSource() == button3) { //if "Request EFT Data Report" button is pressed
 					dispose(); // close current screen
-					//new eftReportScreen(); // open something else screen
+					new EFTReportScreen(); // open EFT Report screen
 				}
 			}    
 		});
@@ -127,7 +64,7 @@ public class ManagerMenu extends JFrame{
 
         JPanel panel = new JPanel(new FlowLayout());
         
-        //add button
+        //adds buttons to screen
         panel.add(button);
         panel.add(button1);
         panel.add(button2);
@@ -138,7 +75,11 @@ public class ManagerMenu extends JFrame{
     }
 
     public static void main(String[] args) {
-        new ManagerMenu();
+        new ManagerMenu(); //opens Manager Menu
+        ProviderFiles.loadProviderMap();
+        MemberFiles.loadMemberMap();
+        ProviderFiles.save();
+        MemberFiles.save();
     }
 }
 
@@ -150,17 +91,20 @@ class ProviderReportScreen extends JFrame {
         super("Provider Report");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         label = new JLabel("Please enter the provider number.");
+        
         t = new JTextField(16);
+        
         button4 = new JButton("Enter");
         button4.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                String s = t.getText();
-                int providerNumber = Integer.parseInt(s);
+                String s = t.getText(); //gets text entered by the user
+                int providerNumber = Integer.parseInt(s); //converts string to an integer
                 Provider provider = ProviderFiles.searchProvider(providerNumber); //searches provider files for provider
                 if(provider != null) { // If provider number is valid, generate the report
                 	dispose(); // close current screen
-					//new GenerateProviderReportScreen(); // open something else screen
+					//new GenerateProviderReportScreen();
                 	JOptionPane.showMessageDialog(ProviderReportScreen.this, "Generating Provider Report for Provider #" + providerNumber);
                 }
                 else {
@@ -169,13 +113,13 @@ class ProviderReportScreen extends JFrame {
             }
         });
         
-        JPanel panel1 = new JPanel(new FlowLayout());
+        JPanel panel = new JPanel(new FlowLayout());
         
-        panel1.add(label);
-        panel1.add(t);
-        panel1.add(button4);
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(t);
+        panel.add(button4);
         
-        add(panel1);
+        add(panel);
         setVisible(true);
     }
 }
@@ -193,12 +137,12 @@ class MemberReportScreen extends JFrame {
         button4 = new JButton("Enter");
         button4.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
-                String s = t.getText();
-                int memberNumber = Integer.parseInt(s);
-                Member member = MemberFiles.searchMember(memberNumber); //searches provider files for provider
-                if(member != null) { // If provider number is valid, generate the report
+                String s = t.getText(); //gets text entered by the user
+                int memberNumber = Integer.parseInt(s); //converts string to an integer
+                Member member = MemberFiles.searchMember(memberNumber); //searches member files for member
+                if(member != null) { // If member number is valid, generate the report
                 	dispose(); // close current screen
-					//new GenerateMemberReportScreen(); // open something else screen
+					//new GenerateMemberReportScreen();
                 	JOptionPane.showMessageDialog(MemberReportScreen.this, "Generating Member Report for Member #" + memberNumber);
                 }
                 else {
@@ -207,14 +151,45 @@ class MemberReportScreen extends JFrame {
             }
         });
         
-        JPanel panel1 = new JPanel(new FlowLayout());
+        JPanel panel = new JPanel(new FlowLayout());
         
-        panel1.add(label);
-        panel1.add(t);
-        panel1.add(button4);
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(t);
+        panel.add(button4);
         
-        add(panel1);
+        add(panel);
         setVisible(true);
     }
     
 }
+
+class SummaryReportScreen extends JFrame{
+	private JLabel label;
+	public SummaryReportScreen() {
+        super("Summary Report");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        label = new JLabel("Generating Summary Report...");
+        
+        add(label);
+
+        setVisible(true);
+    }
+}
+
+class EFTReportScreen extends JFrame{
+	private JLabel label;
+	public EFTReportScreen() {
+        super("EFT Report");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        label = new JLabel("Generating EFT Report...");
+        
+        add(label);
+
+        setVisible(true);
+    }
+}
+
