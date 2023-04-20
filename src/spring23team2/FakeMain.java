6814255b5ce8d3399bdc27368d93673b08be4cf2
juno0013 <jckathiriya@crimson.ua.edu>
2023-04-20 17:04:10 -0500

    Provider report try

--- src/spring23team2/FakeMain.java
+++ src/spring23team2/FakeMain.java
@@ -2,13 +2,19 @@ package spring23team2;
 
 public class FakeMain {
 		public static void main(String[] args) {
-			//ProviderTransactionFiles.loadProviderTransactionMap();
+			ProviderTransactionFiles.loadProviderTransactionMap();
 			MemberTransactionFiles.loadMemberTransactionMap();
 			MemberFiles.loadMemberMap();
+			ProviderFiles.loadProviderMap();
 			
-			MemberReport.createMemberReports();
+			ProviderTransactionFiles.insertProviderTransaction(100000001, "April 20", "April 20", "5:00", 100000007, MemberFiles.searchMember(100000007).getName(), 1001, 50);
+			ProviderTransactionFiles.insertProviderTransaction(100000001, "April 20", "April 20", "5:00", 100000007, MemberFiles.searchMember(100000007).getName(), 1001, 50);
+			ProviderTransactionFiles.insertProviderTransaction(100000002, "April 20", "April 20", "5:00", 100000008, MemberFiles.searchMember(100000008).getName(), 1001, 50);
 			
-			//ProviderTransactionFiles.save();
+			ProviderReport.createProviderReports();
+			
+			ProviderFiles.save();
+			ProviderTransactionFiles.save();
 			MemberFiles.save();
 			MemberTransactionFiles.save();
 		}
