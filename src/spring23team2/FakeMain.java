feb9ddf6b8a3f7d224660278b0a5f3d92f04b3af
juno0013 <jckathiriya@crimson.ua.edu>
2023-04-20 17:04:59 -0500

    fake main

--- src/spring23team2/FakeMain.java
+++ src/spring23team2/FakeMain.java
@@ -7,10 +7,6 @@ public class FakeMain {
 			MemberFiles.loadMemberMap();
 			ProviderFiles.loadProviderMap();
 			
-			ProviderTransactionFiles.insertProviderTransaction(100000001, "April 20", "April 20", "5:00", 100000007, MemberFiles.searchMember(100000007).getName(), 1001, 50);
-			ProviderTransactionFiles.insertProviderTransaction(100000001, "April 20", "April 20", "5:00", 100000007, MemberFiles.searchMember(100000007).getName(), 1001, 50);
-			ProviderTransactionFiles.insertProviderTransaction(100000002, "April 20", "April 20", "5:00", 100000008, MemberFiles.searchMember(100000008).getName(), 1001, 50);
-			
 			ProviderReport.createProviderReports();
 			
 			ProviderFiles.save();
