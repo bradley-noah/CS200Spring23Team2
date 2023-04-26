190ef14ef214683eab8f14af7570653ad903b6ce
Billal Amer <bamer@crimson.ua.edu>
2023-04-26 14:28:19 -0500

    a

--- src/spring23team2/OpMenuTEST.java
+++ src/spring23team2/OpMenuTEST.java
@@ -77,6 +77,7 @@ public class OpMenuTEST extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == upb) {
 					dispose();
+					new UpdateProviderScreen();
 					
 				}
 			}
@@ -146,6 +147,7 @@ class AddMemberScreen extends JFrame {
 				int number = Integer.parseInt(field4.getText());
 				int zip = Integer.parseInt(field5.getText());
 				MemberFiles.insertMember(name, number, address, city, state, zip);
+				MemberFiles.save();
 				dispose();
 				JOptionPane.showMessageDialog(AddMemberScreen.this, "New member created.");
 				MainMenu.main(null);
@@ -191,6 +193,7 @@ class DeleteMemberScreen extends JFrame {
 				Member member = MemberFiles.searchMember(mnum);
 				if(member != null) {
 					MemberFiles.removeMember(mnum);
+					MemberFiles.save();
 					dispose();
 					JOptionPane.showMessageDialog(DeleteMemberScreen.this, "Member " + mnum + " Deleted.");
 					MainMenu.main(null);
@@ -230,7 +233,7 @@ class UpdateMemberScreen extends JFrame {
 				Member member = MemberFiles.searchMember(mnum);
 				if(member != null) {
 					dispose();
-					new UpdateMemberScreen2();
+					new UpdateMemberScreen2(mnum);
 				}
 				else {
 					JOptionPane.showMessageDialog(UpdateMemberScreen.this, "ERROR: Member " + mnum + " Not Found.");
@@ -247,29 +250,20 @@ class UpdateMemberScreen extends JFrame {
 	}
 }
 class UpdateMemberScreen2 extends JFrame {
-	private JButton name, phone, address, city, state, zip;
-	public UpdateMemberScreen2() {
+	private JButton name,address, city, state, zip;
+	private JLabel label;
+	public UpdateMemberScreen2(final int mnum) {
 		super("Update Member");
 		setSize(300,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		label = new JLabel("Select a field to update.");
 		name = new JButton("Member Name");
 		name.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == name) {
 					dispose();
-					new UpdateMemberScreen3();
-					
-				}
-			}
-		
-		});
-		phone = new JButton("Member Phone Number");
-		phone.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == phone) {
-					dispose();
-					new UpdateMemberScreen3();
+					new UpdateMemberScreen3(mnum, 1);
 					
 				}
 			}
@@ -280,7 +274,7 @@ class UpdateMemberScreen2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == address) {
 					dispose();
-					new UpdateMemberScreen3();
+					new UpdateMemberScreen3(mnum, 2);
 					
 				}
 			}
@@ -291,7 +285,7 @@ class UpdateMemberScreen2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == city) {
 					dispose();
-					new UpdateMemberScreen3();
+					new UpdateMemberScreen3(mnum, 3);
 					
 				}
 			}
@@ -302,7 +296,7 @@ class UpdateMemberScreen2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == state) {
 					dispose();
-					new UpdateMemberScreen3();
+					new UpdateMemberScreen3(mnum, 4);
 					
 				}
 			}
@@ -313,32 +307,73 @@ class UpdateMemberScreen2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				if (e.getSource() == zip) {
 					dispose();
-					new UpdateMemberScreen3();
+					new UpdateMemberScreen3(mnum, 5);
 					
 				}
 			}
 		
 		});
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(name);
+        panel.add(address);
+        panel.add(city);
+        panel.add(state);
+        panel.add(zip);
+        add(panel);
+        setVisible(true);
 	}
 }
 class UpdateMemberScreen3 extends JFrame {
 	private JLabel label;
 	private JTextField field;
 	private JButton b;
-	public UpdateMemberScreen3() {
+	public UpdateMemberScreen3(final int mnum, final int info) {
 		super("Update Member");
 		setSize(300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		label = new JLabel("Enter updated member information:");
-		
 		field = new JTextField(16);
 		b = new JButton("Enter");
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Member member = MemberFiles.searchMember(mnum);
+				String name = member.getName();				
+				String address = member.getAddress();
+				String city = member.getCity();
+				String state = member.getState();
+				int zip = member.getZip();
+				int number = member.getMemberNumber();
 				String s = field.getText();
+				if (info == 1) {
+					MemberFiles.removeMember(number);
+					MemberFiles.insertMember(s, number, address, city, state, zip);
+					MemberFiles.save();
+				}
+				if (info == 2) {
+					MemberFiles.removeMember(number);
+					MemberFiles.insertMember(name, number, s, city, state, zip);
+					MemberFiles.save();
+				}
+				if (info == 3) {
+					MemberFiles.removeMember(number);
+					MemberFiles.insertMember(name, number, address, s, state, zip);
+					MemberFiles.save();
+				}
+				if (info == 4) {
+					MemberFiles.removeMember(number);
+					MemberFiles.insertMember(name, number, address, city, s, zip);
+					MemberFiles.save();
+				}
+				if (info == 5) {
+					int zipcode = Integer.parseInt(s);
+					MemberFiles.removeMember(number);
+					MemberFiles.insertMember(name, number, address, city, state, zipcode);
+					MemberFiles.save();
+				}
 				dispose();
 				JOptionPane.showMessageDialog(UpdateMemberScreen3.this, "Member Information Updated.");
+				MainMenu.main(null);
 			}
 			
 		});
@@ -382,6 +417,7 @@ class AddProviderScreen extends JFrame {
 				int number = Integer.parseInt(field4.getText());
 				int zip = Integer.parseInt(field5.getText());
 				ProviderFiles.insertProvider(name, number, address, city, state, zip);
+				ProviderFiles.save();
 				dispose();
 				JOptionPane.showMessageDialog(AddProviderScreen.this, "New provider created.");
 				MainMenu.main(null);
@@ -426,6 +462,8 @@ class DeleteProviderScreen extends JFrame {
 				int pnum = Integer.parseInt(s);
 				Provider provider = ProviderFiles.searchProvider(pnum);
 				if(provider != null) {
+					ProviderFiles.removeProvider(pnum);
+					ProviderFiles.save();
 					dispose();
 					JOptionPane.showMessageDialog(DeleteProviderScreen.this, "Provider " + pnum + " Deleted.");
 				}
@@ -442,5 +480,177 @@ class DeleteProviderScreen extends JFrame {
         add(panel);
         setVisible(true);
 		
+	}
+}
+class UpdateProviderScreen extends JFrame {
+	private JLabel label;
+	private JTextField field;
+	private JButton b;
+	public UpdateProviderScreen() {
+		super("Update Provider");
+		setSize(300, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		label = new JLabel("Enter provider number of provider to update:");
+		
+		field = new JTextField(16);
+		b = new JButton("Enter");
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String s = field.getText();
+				int pnum = Integer.parseInt(s);
+				Provider provider = ProviderFiles.searchProvider(pnum);
+				if(provider != null) {
+					dispose();
+					new UpdateMemberScreen2(pnum);
+				}
+				else {
+					JOptionPane.showMessageDialog(UpdateProviderScreen.this, "ERROR: Member " + pnum + " Not Found.");
+				}
+			}
+			
+		});
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(field);
+        panel.add(b);
+        add(panel);
+        setVisible(true);
+	}
+}
+class UpdateProviderScreen2 extends JFrame {
+	private JButton name,address, city, state, zip;
+	private JLabel label;
+	public UpdateProviderScreen2(final int pnum) {
+		super("Update Provider");
+		setSize(300,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		label = new JLabel("Select a field to update.");
+		name = new JButton("Provider Name");
+		name.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == name) {
+					dispose();
+					new UpdateMemberScreen3(pnum, 1);
+					
+				}
+			}
+		
+		});
+		address = new JButton("Provider Address");
+		address.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == address) {
+					dispose();
+					new UpdateMemberScreen3(pnum, 2);
+					
+				}
+			}
+		
+		});
+		city = new JButton("Provider City");
+		city.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == city) {
+					dispose();
+					new UpdateMemberScreen3(pnum, 3);
+					
+				}
+			}
+		
+		});
+		state = new JButton("Provider State");
+		state.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == state) {
+					dispose();
+					new UpdateMemberScreen3(pnum, 4);
+					
+				}
+			}
+		
+		});
+		zip = new JButton("Provider ZIP Code");
+		zip.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == zip) {
+					dispose();
+					new UpdateMemberScreen3(pnum, 5);
+					
+				}
+			}
+		
+		});
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(name);
+        panel.add(address);
+        panel.add(city);
+        panel.add(state);
+        panel.add(zip);
+        add(panel);
+        setVisible(true);
+	}
+}
+class UpdateProviderScreen3 extends JFrame {
+	private JLabel label;
+	private JTextField field;
+	private JButton b;
+	public UpdateProviderScreen3(final int mnum, final int info) {
+		super("Update Provider");
+		setSize(300, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		label = new JLabel("Enter updated Provider information:");
+		field = new JTextField(16);
+		b = new JButton("Enter");
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Provider provider = ProviderFiles.searchProvider(mnum);
+				String name = provider.getName();				
+				String address = provider.getAddress();
+				String city = provider.getCity();
+				String state = provider.getState();
+				int zip = provider.getZip();
+				int number = provider.getProviderNumber();
+				String s = field.getText();
+				if (info == 1) {
+					ProviderFiles.removeProvider(number);
+					ProviderFiles.insertProvider(s, number, address, city, state, zip);
+					ProviderFiles.save();
+				}
+				if (info == 2) {
+					ProviderFiles.removeProvider(number);
+					ProviderFiles.insertProvider(name, number, s, city, state, zip);
+					ProviderFiles.save();
+				}
+				if (info == 3) {
+					ProviderFiles.removeProvider(number);
+					ProviderFiles.insertProvider(name, number, address, s, state, zip);
+					ProviderFiles.save();
+				}
+				if (info == 4) {
+					ProviderFiles.removeProvider(number);
+					ProviderFiles.insertProvider(name, number, address, city, s, zip);
+					ProviderFiles.save();
+				}
+				if (info == 5) {
+					int zipcode = Integer.parseInt(s);
+					ProviderFiles.removeProvider(number);
+					ProviderFiles.insertProvider(name, number, address, city, state, zipcode);
+					ProviderFiles.save();
+				}
+				dispose();
+				JOptionPane.showMessageDialog(UpdateProviderScreen3.this, "Provider Information Updated.");
+				MainMenu.main(null);
+			}
+			
+		});
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(label);        //adds label, text field, and button to screen
+        panel.add(field);
+        panel.add(b);
+        add(panel);
+        setVisible(true);
 	}
 }
