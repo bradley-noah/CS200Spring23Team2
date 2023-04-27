702e5d7e69e83f951998bef98fa3e66e3638ea43
gadeterding <gadeterding@crimson.ua.edu>
2023-04-27 17:15:51 -0500

    removed save from provider directory

--- src/spring23team2/ProviderDirectory.java
+++ src/spring23team2/ProviderDirectory.java
@@ -34,29 +34,6 @@ public class ProviderDirectory {
 	    }
 
 	    /**
-	     * Writes all data from Map to a file data.properties
-	     * @throws IOException
-	     * @throws FileNotFoundException
-	     */
-	    public static void save() {
-	        Properties prop = new Properties();
-	        for (Map.Entry<Integer,String[]> entry : ProviderDirectoryMap.entrySet()) {
-	            int serviceCode = entry.getKey();
-	            String[] values = entry.getValue();
-	            String data = values[0] + "," + values[1];
-	            prop.setProperty(Integer.toString(serviceCode), data);
-	    }
-	        try {
-	            FileOutputStream fileOut = new FileOutputStream("ProviderDirectory.properties");
-	            prop.store(fileOut, "Provider Directory Map");
-	            fileOut.close();
-	            System.out.println("Provider Directory saved successfully.");
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-
-	    /**
 	     * Writes all data from file to Map
 	     * @throws IOException
 	     * @throws FileNotFoundException
